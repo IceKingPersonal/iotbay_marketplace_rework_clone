ab419ddeb1bba7a7e1e267160c650608d58a840f
IceKingPersonal <icecolin.rk@gmail.com>
2026-05-25 21:58:21 +1000

    added frontend_rework

---
+++ frontend_rework/src/main.jsx
@@ -0,0 +1,17 @@
+//Entry point for react app. Renders the app.jsx into the root element in index.html.
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
